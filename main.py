db261abd3482b5bb4d6b36108d06e81bcd256fdf
Beau <beau7476@gmail.com>
2023-01-04 21:32:38 +0000

    Made it not suck

--- main.py
+++ main.py
@@ -1,70 +1,71 @@
 import os
 
+repoUrl = "https://github.com/j1165-mods/project"
 
-# ask the user for the directory of their minecraft foledr
+
+# ask the user for the directory of their minecraft folder
 # give them the option to enter it manually or to use the default
-# (the default is the folder that contains the .minecraft folder)
-
 
 def get_minecraft_folder():
     while True:
-        print("Where is your minecraft folder? (default is .minecraft)")
+        print("Where is your Minecraft mods folder? (default is .minecraft/mods)")
         print("1. Default")
         print("2. Enter manually")
 
-        choice = input("Enter your choice: ")
+        choice = input()
 
         if choice == "1":
-            path = os.path.join(os.path.expanduser("~"), ".minecraft")
+            path = os.path.join(os.path.expanduser("~"), ".minecraft/mods")
             # print the path to console and ask if it is correct
-            # if it is correct, return the path
-            # if it is not correct, ask again
 
-            print(f"Your minecraft folder is {path}")
+            print(f"Your Minecraft mods folder is {path}")
             print("Is this correct?")
 
             if input("Enter y/n: ") == "y":
                 return path
 
         elif choice == "2":
-            return input("Enter the path to your minecraft folder: ")
-
+            return input("Enter the path to your Minecraft mods folder: ")
         else:
             print("Invalid choice")
 
 
-# git clone https://github.com/j1165-mods/project into the folder with the name "mods"
-# (the folder is created if it doesn't exist)
-
-def install_mods(minecraft_folder):
-    mods_folder = os.path.join(minecraft_folder, "mods")
-    if not os.path.exists(mods_folder):
-        os.mkdir(mods_folder)
-
-    os.system(f"cd {mods_folder} && git clone https://github.com/j1165-mods/project")
+mcFolder = get_minecraft_folder()
 
 
-# run the program
-minecraft_folder = get_minecraft_folder()
-print(f"Installing mods to {minecraft_folder}")
+def download_repo():
+    print("Downloading repo...")
+    print("Sit tight.")
+    # download the repo
+    os.system(f"git clone {repoUrl}")
 
-# Check if the folder exists and is empty
-# if it is empty, install the mods
 
-if os.path.exists(minecraft_folder):
-    if not os.listdir(minecraft_folder):
-        install_mods(minecraft_folder)
-    else:
-        print("The folder is not empty")
-        print("Should we erase the folder? (y/n)")
-        if input() == "y":
-            os.system(f"rm -rf {minecraft_folder}")
-            os.system(f"mkdir {minecraft_folder}")
+def install_repo():
 
-            print("Folder erased!")
+    print("The repo has been downloaded.")
+    print("Installing... This shouldn't take long.")
 
-            install_mods(minecraft_folder)
-        else:
-            print("Exiting")
+    # copy the files from the repo to the minecraft mods folder
+    # move the repo to the mods folder
+    os.system(f"mv project {mcFolder}")
+    print("Moved repo to mods folder.")
 
-install_mods(minecraft_folder)
+    # copy the contents one level up
+    os.system(f"mv {mcFolder}/project/* {mcFolder}")
+    os.system(f"rm -r {mcFolder}/project")
+    print("Copied contents to mods folder")
+
+    # delete the repo
+    os.system("rm -rf project")
+    print("Deleted unneeded repo.")
+
+    print("Done!")
+
+
+def main():
+    download_repo()
+    install_repo()
+
+
+main()
+
